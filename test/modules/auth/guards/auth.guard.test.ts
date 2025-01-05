e678c8393c7005ae44d76b5869c6f19deb4a5417
chaicopadillag <xt9738@rimac.com.pe>
2025-01-05 18:08:32 -0500

    add unit tests for auth guard to verify routing behavior and localStorage interactions

--- test/modules/auth/guards/auth.guard.test.ts
+++ test/modules/auth/guards/auth.guard.test.ts
@@ -1,5 +1,42 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { authGuard } from '@/modules/auth/guards/auth.guard';
+import type { RouteLocationNormalizedGeneric } from 'vue-router';
+
 describe('Auth Guard', () => {
-  it('should be defined', () => {
-    expect(true).toBeTruthy();
+  const to: RouteLocationNormalizedGeneric = {
+    name: 'contact',
+    params: {},
+    matched: [],
+    fullPath: '',
+    query: {},
+    hash: '',
+    redirectedFrom: undefined,
+    path: '/contact',
+    meta: {},
+  };
+
+  const from: any = {};
+  const next = vi.fn();
+
+  // beforeEach(() => {
+  //   localStorage.clear();
+  // });
+
+  it('should vue router guard', async () => {
+    await authGuard(to, from, next);
+    vi.spyOn(Storage.prototype, 'getItem').mockReturnValue(null);
+    expect(next).toHaveBeenCalledWith({ replace: true, name: 'login' });
+  });
+
+  it('should called localStorage.setItem', async () => {
+    const setItemSpy = vi.spyOn(Storage.prototype, 'setItem');
+    await authGuard(to, from, next);
+    expect(setItemSpy).toHaveBeenCalledWith('lastPath', '/contact');
+  });
+
+  it('should not call next when token is not present', async () => {
+    vi.spyOn(Storage.prototype, 'getItem').mockReturnValue(Date.now().toString());
+    await authGuard(to, from, next);
+    expect(next).toHaveBeenCalledWith();
   });
 });
