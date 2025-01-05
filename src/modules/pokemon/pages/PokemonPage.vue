f9fc5d5dcd76cbc014a2b0bbfce79b61973fdec0
chaicopadillag <xt9738@rimac.com.pe>
2025-01-05 14:35:53 -0500

    add authentication and landing layouts, pages, and routing

---
+++ src/modules/pokemon/pages/PokemonPage.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="flex flex-col items-center">
+    <h1 class="text-3xl font-bold">
+      Pokemon N° <span class="text-blue-500">{{ id }}</span>
+    </h1>
+    <div class="mt-4">
+      <img
+        :src="`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${id}.svg`"
+        :alt="`Pokemon N° ${id}`"
+        class="w-48 h-48"
+      />
+    </div>
+    <RouterLink
+      :to="{ name: 'pokemon', params: { id: id + 1 } }"
+      class="mt-4 text-white hover:underline p-2 rounded-md bg-blue-500 hover:bg-blue-700"
+    >
+      Siguiente Pokemon
+    </RouterLink>
+  </div>
+</template>
+
+<script lang="ts" setup>
+type Props = {
+  id: number;
+};
+const props = defineProps<Props>();
+
+console.log(props.id);
+</script>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,21 @@
+import NotFoundPage from '@/modules/common/pages/NotFoundPage.vue';
 import HomePage from '@/modules/landing/pages/HomePage.vue';
 import { createRouter, createWebHistory, type RouteRecordRaw } from 'vue-router';
 
-const routes: RouteRecordRaw[] = [
+const navAuthRoutes: RouteRecordRaw[] = [
+  {
+    path: 'login',
+    name: 'login',
+    component: () => import('@/modules/auth/pages/LoginPage.vue'),
+  },
+  {
+    path: 'register',
+    name: 'register',
+    component: () => import('@/modules/auth/pages/RegisterPage.vue'),
+  },
+];
+
+const linkRoutes: RouteRecordRaw[] = [
   {
     path: '/',
     name: 'home',
@@ -22,6 +36,39 @@ const routes: RouteRecordRaw[] = [
     name: 'contact',
     component: () => import('@/modules/landing/pages/ContactPage.vue'),
   },
+  {
+    path: '/pokemon/:id',
+    name: 'pokemon',
+    props: (route) => {
+      const { id = '1' } = route.params as { id: string };
+
+      const idParse = isNaN(+id) ? 1 : Number(route.params.id);
+
+      return { id: idParse };
+    },
+    component: () => import('@/modules/pokemon/pages/PokemonPage.vue'),
+  },
+  ...navAuthRoutes.map((route) => ({
+    ...route,
+    path: `/auth/${route.path}`,
+  })),
+];
+
+const routes: RouteRecordRaw[] = [
+  {
+    path: '/',
+    component: () => import('@/modules/landing/layouts/LandingLayout.vue'),
+    children: linkRoutes,
+  },
+  {
+    path: '/auth',
+    name: 'auth',
+    redirect: { name: 'login' },
+    component: () => import('@/modules/auth/layouts/AuthLoyout.vue'),
+    children: navAuthRoutes,
+  },
+
+  { path: '/:pathMatch(.*)*', name: 'NotFound', component: NotFoundPage },
 ];
 
 export const router = createRouter({
@@ -29,7 +76,7 @@ export const router = createRouter({
   routes,
 });
 
-export const linkRoutes = routes.map((route) => ({
-  path: route.path,
+export const navLinkRoutes = linkRoutes.map((route) => ({
   name: route.name,
+  path: route.path,
 }));
